2e38db29c4b1a891a99e355126f5371919a93941
pranjal-caizin <pranjal@caizin.com>
2026-03-28 23:29:30 +0530

    Initial Commit

---
+++ src/main/java/com/recommendationengine/RecommendationEngineApplication.java
@@ -0,0 +1,12 @@
+package com.recommendationengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationEngineApplication.class, args);
+    }
+}
